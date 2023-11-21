65375c38641422f08d5994e769caede47e77d3ee
HumbertoSifontes <132623264+HumbertoSifontes@users.noreply.github.com>
2023-11-21 16:47:24 -0300

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,12 +9,6 @@ const Inicio = () => {
     <div className="container-fluid homepage">
       <div className="row m-0">
         <div className="col-md-12 p-0 pt-4 pb-4 homevideo">
-              <p className='hometext'>Particpantes: <hr /> 
-               Humberto Sifontes <hr />
-               Ian Prazuch <hr />
-               Mateo Saldías <hr /> 
-               De Bernardi Constanza <hr /> 
-               Micaela Pérez</p>
               <Video width={600} height={400} autoPlay loop>
                 <source src={VideoApp} type='video/mp4'/>
               </Video>
